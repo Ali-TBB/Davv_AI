6c252f4175c2228c72e5ca1dea91ce82147d23dd
AbdoPrDZ <abdopr47@gmail.com>
2024-08-06 19:40:21 +0100

    Add target selection functionality in main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 
+import questionary
+
 from utils.env import Env
 
 
@@ -20,15 +22,35 @@ def arg(index, default=None):
         return default
 
 
+def ask_target():
+    """
+    Ask the user to select a target.
+
+    Returns:
+        The selected target.
+    """
+    return questionary.select(
+        "Select a target",
+        choices=[
+            "Console",
+            "Browser",
+        ],
+    ).ask()
+
+
 if __name__ == "__main__":
     Env.init()
 
-    target = arg(1, "console")
-    if target == "console":
+    target = arg(1)
+
+    if not target:
+        target = ask_target()
+
+    if target.lower() == "console":
         from run import console
 
         console.AICommand().cmdloop()
-    elif target == "browser":
+    elif target.lower() == "browser":
         from run import browser
 
         browser.start()
